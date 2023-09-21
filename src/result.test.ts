558e31f12c7f4ab26af99398c33c655e426a9979
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-21 07:50:30 -0300

    chore: tests map fn

--- src/result.test.ts
+++ src/result.test.ts
@@ -1,44 +1,60 @@
-import {describe} from "vitest"
-import {Err, Ok, isErr, isOk, unwrap, unwrapErr, Result, unwrapOr} from "./result"
+import { describe } from 'vitest';
+import {
+  Result,
+  isErr,
+  isOk,
+  map,
+  unwrap,
+  unwrapErr,
+  unwrapOr,
+} from './result';
 
-describe("result", (it) => {
-  it("should be ok", ({expect}) => {
-    const result: Result<number, string> = {type: "ok", value: 1}
-    expect(isOk(result)).toBe(true)
-  })
+describe('result', (it) => {
+  it('should be ok', ({ expect }) => {
+    const result: Result<number, string> = { type: 'ok', value: 1 };
+    expect(isOk(result)).toBe(true);
+  });
 
-  it("should be err", ({expect}) => {
-    const result: Result<number, string> = {type: "err", error: "error"}
-    expect(isErr(result)).toBe(true)
-  })
+  it('should be err', ({ expect }) => {
+    const result: Result<number, string> = { type: 'err', error: 'error' };
+    expect(isErr(result)).toBe(true);
+  });
 
-  it("should unwrap ok", ({expect}) => {
-    const result: Result<number, string> = {type: "ok", value: 1}
-    expect(unwrap(result)).toBe(1)
-  })
+  it('should unwrap ok', ({ expect }) => {
+    const result: Result<number, string> = { type: 'ok', value: 1 };
+    expect(unwrap(result)).toBe(1);
+  });
 
-  it("should unwrap err", ({expect}) => {
-    const result: Result<number, string> = {type: "err", error: "error"}
-    expect(unwrapErr(result)).toBe("error")
-  })
+  it('should unwrap err', ({ expect }) => {
+    const result: Result<number, string> = { type: 'err', error: 'error' };
+    expect(unwrapErr(result)).toBe('error');
+  });
 
-  it("should return default value on unwrapOr with err", ({expect}) => {
-    const result: Result<number, string> = {type: "err", error: "error"}
-    expect(unwrapOr(result, 1)).toBe(1)
-  })
+  it('should return default value on unwrapOr with err', ({ expect }) => {
+    const result: Result<number, string> = { type: 'err', error: 'error' };
+    expect(unwrapOr(result, 1)).toBe(1);
+  });
 
-  it("should return value on unwrapOr with ok", ({expect}) => {
-    const result: Result<number, string> = {type: "ok", value: 1}
-    expect(unwrapOr(result, 2)).toBe(1)
-  })
+  it('should return value on unwrapOr with ok', ({ expect }) => {
+    const result: Result<number, string> = { type: 'ok', value: 1 };
+    expect(unwrapOr(result, 2)).toBe(1);
+  });
 
-  it("should throw error on unwrapErr with ok", ({expect}) => {
-    const result: Result<number, string> = {type: "ok", value: 1}
-    expect(() => unwrapErr(result)).toThrow()
-  })
+  it('should throw error on unwrapErr with ok', ({ expect }) => {
+    const result: Result<number, string> = { type: 'ok', value: 1 };
+    expect(() => unwrapErr(result)).toThrow();
+  });
 
-  it("should throw value on unwrap with err", ({expect}) => {
-    const result: Result<number, string> = {type: "err", error: "error"}
-    expect(() => unwrap(result)).toThrow()
-  })
-})
+  it('should throw value on unwrap with err', ({ expect }) => {
+    const result: Result<number, string> = { type: 'err', error: 'error' };
+    expect(() => unwrap(result)).toThrow();
+  });
+
+  it('should map ok', ({ expect }) => {
+    const result: Result<number, string> = { type: 'ok', value: 1 };
+    expect(map(result, (value) => value * 10)).toStrictEqual({
+      type: 'ok',
+      value: 10,
+    });
+  });
+});
